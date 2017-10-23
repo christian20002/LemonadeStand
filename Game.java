a218754e2527de2fd522960e231cdc8ecedcefd5
Mr. Frog <christian20002@users.noreply.github.com>
2017-10-23 12:37:36 -0700

    Removed example function from original bluej file

--- Game.java
+++ Game.java
@@ -130,9 +130,4 @@ public class Game
      * @param  y  a sample parameter for a method
      * @return    the sum of x and y
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
